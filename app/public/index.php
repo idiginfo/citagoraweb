fe9523e8f858bde746a3a02347334a4a7edb3258
Casey McLaughlin <caseyamcl@gmail.com>
2012-08-07 10:49:05 -0400

    Citagora PHP application skeleton

---
+++ app/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Citagora Web/Service Layer Index File
+ *
+ * @package Citagora
+ * @subpackage Citagora Web Service
+ * @author Casey McLaughlin
+ * @copyright (c) 2012 Florida State University
+ */
+
+/* EOF: index.php */
