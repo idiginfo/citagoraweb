7e98cc91e62b21c9cc6295006fbff1943460c86c
Casey McLaughlin <caseyamcl@gmail.com>
2013-01-16 13:59:08 -0500

    Added sanity check to index.php file

--- web/index.php
+++ web/index.php
@@ -3,6 +3,11 @@
 //Autoload
 require(__DIR__ . '/../vendor/autoload.php');
 
+//Friendly error message to avoid WSOD
+if ( ! is_readable(__DIR__ . '/../vendor/autoload.php')) {
+    die('Missing vendor dependencies.  Did you forget to run setup?');
+}
+
 //Mode not previously set by dev.php or maint.php?
 if ( ! isset($mode)) {
     $mode = 'production';
